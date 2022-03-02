60c5b09a0dae836975b137552f3d092eaf4a9c19
AdrianValladares <a.valladares.d@gmail.com>
2022-03-02 19:20:39 +0100

    wrote 404 error

--- src/server/middlewares/notFoundError.js
+++ src/server/middlewares/notFoundError.js
@@ -2,6 +2,11 @@ const debug = require("debug")("twitter-app:errors:not-found");
 
 const notFoundError = (req, res, next) => {
   debug("Request received for unexpected endpoint");
+  try {
+    res.status(404).json({ error: true, message: "endpoint not found" });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = notFoundError;
