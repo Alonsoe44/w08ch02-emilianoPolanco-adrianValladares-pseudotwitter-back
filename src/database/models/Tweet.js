fbf59a36b7f6099749acb331b97af097cb9c201b
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-02 19:26:30 +0100

    Creating a schema for a Tuit

--- src/database/models/Tweet.js
+++ src/database/models/Tweet.js
@@ -1,3 +1,24 @@
-const { Schema } = require("monggose");
+const { Schema } = require("mongoose");
 
-const TweetSchema = new Schema();
+const TweetSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  text: {
+    type: String,
+    minlength: 1,
+    maxlength: 200,
+    required: true,
+  },
+
+  likes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Tuit = model("Tweet", TweetSchema, "tuits");
+
+module.exports = Tuit;
